aa98e50570daf2bf961e8c84c3e7b72b0173fa4b
prets <fritz.gerald.dumdum@sun-asterisk.com>
2022-04-17 21:58:03 +0800

    handle qty for stock not in wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,5 +1,6 @@
 class Wallet < ApplicationRecord
   validate :validate_stock
+  validate :validate_presence_of_qty
   validate :validate_qty
 
   belongs_to :user, optional: true
@@ -7,17 +8,27 @@ class Wallet < ApplicationRecord
 
   def validate_stock
     if stock.nil?
-      errors.add(:stock_id, 'Please select a stock.')
+      return errors.add(:stock_id, 'Please select a stock.')
+    end
+  end
+
+  def validate_presence_of_qty
+    if qty.nil?
+      return errors.add(:qty, 'Quantity is required.')
     end
   end
 
   def validate_qty
-    if stock.present?
+    if stock.present? && !qty.nil?
       if qty > stock.qty && @@action_type == 'BUY'
         return errors.add(:qty, "Invalid Quantity. Remaining stock shares for #{stock.name} is #{stock.qty}.")
       else
-        if @@action_type == 'SELL' && qty > stock.wallets[0].qty
-          return errors.add(:qty, "Invalid Quantity. Your remaining stock shares for #{stock.name} is only #{stock.wallets[0].qty}.")
+        if stock.wallets.present?
+          if @@action_type == 'SELL' && qty > stock.wallets[0].qty
+            return errors.add(:qty, "Invalid Quantity. Your remaining stock shares for #{stock.name} is only #{stock.wallets[0].qty}.")
+          end
+        else
+          return errors.add(:stock_id, "You dont have shares to #{stock.name}.")
         end
       end
     end
