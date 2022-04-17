77c5e92e132cf82ebd443841743b9d0cc6eef9f1
Jerick Borela <jerickborela24@gmail.com>
2022-04-17 19:45:38 +0800

    mailers

--- app/mailers/accepted_mailer.rb
+++ app/mailers/accepted_mailer.rb
@@ -1,12 +1,8 @@
 class AcceptedMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.accepted_mailer.send_acceptance_letter.subject
-  #
-  def send_acceptance_letter
-    @user = user
-    mail to: @user.email, subject: "Registration to Stock App has been Accepted", from: "admin@admin.com"
-  end
+    def send_acceptance_email(user)
+        @user = user
+        mail to: @user.email, subject: "Registration to Stock App has been Accepted", from: "admin@admin.com"
+    end
+
 end
